bfb6de73f422a72c1735f6f4c56c38aab13c857b
lakshmanvirijala1 <lvirijala@gmail.com>
2025-08-22 23:26:50 +0530

    Added CI/CD workflow

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello DevOps CI/CD 🚀");
+  res.send("Hello DevOps CI/CD 🚀 okay good");
 });
 
 app.listen(3000, () => {
